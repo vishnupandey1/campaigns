da0dbe14c124306bcd8ff18b278e58c949fa67d2
vishnupandey1 <pandeyuma2011@gmail.com>
2021-03-27 18:28:43 +0530

    Fix timer logic

--- src/campaigns/utils/helper.js
+++ src/campaigns/utils/helper.js
@@ -1,20 +1,17 @@
-import moment from "moment"
+import moment from "moment";
 
 export const getCurrentLanguage = () => {
   let localLang = window.localStorage.getItem("i18nextLng");
   return localLang ? localLang.split("-")[0] : "en";
 };
 
-export const isMobile = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
+export const isMobile = /iPhone|iPad|iPod|Android/i.test(
+  window.navigator.userAgent
+);
 
 export const getTime = (time) => {
   if (!time) return;
   let createdAt;
-  const today = new Date();
-  if (new Date(time).toDateString() > today.toDateString()) {
-    createdAt = moment(new Date(time)).fromNow();
-  } else {
-    createdAt = moment(new Date(time)).format("MMM DD, h:mm A").toLowerCase();
-  }
+  createdAt = moment(new Date(time)).fromNow();
   return createdAt;
 };
